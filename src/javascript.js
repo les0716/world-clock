3864a50d12669698f75f33ba94502f5b4630cd17
les0716 <141790794+les0716@users.noreply.github.com>
2023-09-17 21:38:09 -0700

    added honolulu city

--- src/javascript.js
+++ src/javascript.js
@@ -25,6 +25,20 @@ function updateParisTime() {
     );
   }
 }
+function updateHonoluluTime() {
+  let honoluluElement = document.querySelector("#honolulu");
+  if (honoluluElement) {
+    let honoluluDateElement = honoluluElement.querySelector(".date");
+    let honoluluTimeElement = honoluluElement.querySelector(".time");
+    let honoluluTime = moment().tz("Pacific/Honolulu");
+
+    honoluluDateElement.innerHTML = honoluluTime.format("MMMM	Do YYYY");
+    honoluluTimeElement.innerHTML = honoluluTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   let cityName = cityTimeZone.split("/")[1];
